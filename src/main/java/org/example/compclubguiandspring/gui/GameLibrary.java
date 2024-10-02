9872674f4ecc1c5526868d955f0b0ac43d2cf804
NeironEssensive <aglushak10@gmail.com>
2024-10-02 13:55:55 +0300

    Added more activites and logo buttons

--- src/main/java/org/example/compclubguiandspring/gui/GameLibrary.java
+++ src/main/java/org/example/compclubguiandspring/gui/GameLibrary.java
@@ -7,10 +7,33 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class GameLibrary extends JFrame implements ActionListener {
+    JLabel balance;
+    JLabel balanceText;
+    JButton balanceAddButton;
     private Container c;
     ImageIcon icon = new ImageIcon("C:\\Users\\aglus\\IdeaProjects\\CompClubGUIandSpring\\src\\main\\resources\\photo_2024-09-28_16-30-31 (1).jpg");
+    ImageIcon iconSteam = new ImageIcon("C:\\Users\\aglus\\IdeaProjects\\CompClubGUIandSpring\\src\\main\\resources\\Steam_icon_logo.svg.png");
+    ImageIcon iconDota = new ImageIcon("C:\\Users\\aglus\\IdeaProjects\\CompClubGUIandSpring\\src\\main\\resources\\dotalogo0.png");
+    ImageIcon iconCS = new ImageIcon("C:\\Users\\aglus\\IdeaProjects\\CompClubGUIandSpring\\src\\main\\resources\\cslogo0.jpg");
+    ImageIcon iconBrawlHalla = new ImageIcon("C:\\Users\\aglus\\IdeaProjects\\CompClubGUIandSpring\\src\\main\\resources\\brawlhallalogo0.png");
+    ImageIcon iconDBD = new ImageIcon("C:\\Users\\aglus\\IdeaProjects\\CompClubGUIandSpring\\src\\main\\resources\\deadbydaylightlogo0.png");
+    ImageIcon iconValorant = new ImageIcon("C:\\Users\\aglus\\IdeaProjects\\CompClubGUIandSpring\\src\\main\\resources\\valorantlogo0.png");
+    ImageIcon iconFortnite = new ImageIcon("C:\\Users\\aglus\\IdeaProjects\\CompClubGUIandSpring\\src\\main\\resources\\fortnitelogo0.jpg");
+    ImageIcon iconLOL = new ImageIcon("C:\\Users\\aglus\\IdeaProjects\\CompClubGUIandSpring\\src\\main\\resources\\lollogo0.jpg");
+    ImageIcon iconDiscord = new ImageIcon("C:\\Users\\aglus\\IdeaProjects\\CompClubGUIandSpring\\src\\main\\resources\\discordlogo0.jpg");
+    ImageIcon iconGHub = new ImageIcon("C:\\Users\\aglus\\IdeaProjects\\CompClubGUIandSpring\\src\\main\\resources\\ghublogo0.jpg");
+
     JLabel logotype = new JLabel(icon);
     private JButton launchSteamButton;
+    private JButton launchDotaButton;
+    private JButton launchBrawlhallaButton;
+    private JButton launchCounterStrikeButton;
+    private JButton launchDBDButton;
+    private JButton launchValorantButton;
+    private JButton launchFortniteButton;
+    private JButton launchLOLButton;
+    private JButton launchDiscordButton;
+    private JButton launchGHubButton;
     GameLibrary(){
         setTitle("InvokerQWE");
         setBounds(300, 90, 1200, 800);
@@ -24,12 +47,95 @@ public class GameLibrary extends JFrame implements ActionListener {
         c.add(logotype);
 
 
-        launchSteamButton = new JButton("Steam");
+        launchSteamButton = new JButton("Steam", iconSteam);
         launchSteamButton.setFont(new Font("Arial", Font.PLAIN, 15));
-        launchSteamButton.setSize(150, 40);
+        launchSteamButton.setSize(175, 65); //icon 50x50
         launchSteamButton.setLocation(500, 500);
         launchSteamButton.addActionListener(this);
         c.add(launchSteamButton);
+
+
+        launchDotaButton = new JButton("Dota 2", iconDota);
+        launchDotaButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        launchDotaButton.setSize(175, 65); //icon 50x50
+        launchDotaButton.setLocation(300, 500);
+        launchDotaButton.addActionListener(this);
+        c.add(launchDotaButton);
+
+        launchBrawlhallaButton = new JButton("BrawlHalla", iconBrawlHalla);
+        launchBrawlhallaButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        launchBrawlhallaButton.setSize(175, 65); //icon 50x50
+        launchBrawlhallaButton.setLocation(700, 500);
+        launchBrawlhallaButton.addActionListener(this);
+        c.add(launchBrawlhallaButton);
+
+        launchCounterStrikeButton = new JButton("CS2", iconCS);
+        launchCounterStrikeButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        launchCounterStrikeButton.setSize(175, 65); //icon 50x50
+        launchCounterStrikeButton.setLocation(100, 500);
+        launchCounterStrikeButton.addActionListener(this);
+        c.add(launchCounterStrikeButton);
+
+        launchDBDButton = new JButton("DBD", iconDBD);
+        launchDBDButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        launchDBDButton.setSize(175, 65); //icon 50x50
+        launchDBDButton.setLocation(900, 500);
+        launchDBDButton.addActionListener(this);
+        c.add(launchDBDButton);
+
+        launchValorantButton = new JButton("Valorant", iconValorant);
+        launchValorantButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        launchValorantButton.setSize(175, 65); //icon 50x50
+        launchValorantButton.setLocation(500, 600);
+        launchValorantButton.addActionListener(this);
+        c.add(launchValorantButton);
+
+        launchFortniteButton = new JButton("Fortnite", iconFortnite);
+        launchFortniteButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        launchFortniteButton.setSize(175, 65); //icon 50x50
+        launchFortniteButton.setLocation(300, 600);
+        launchFortniteButton.addActionListener(this);
+        c.add(launchFortniteButton);
+
+        launchLOLButton = new JButton("LoL", iconLOL);
+        launchLOLButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        launchLOLButton.setSize(175, 65); //icon 50x50
+        launchLOLButton.setLocation(700, 600);
+        launchLOLButton.addActionListener(this);
+        c.add(launchLOLButton);
+
+        launchDiscordButton = new JButton("Discord", iconDiscord);
+        launchDiscordButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        launchDiscordButton.setSize(175, 65); //icon 50x50
+        launchDiscordButton.setLocation(100, 600);
+        launchDiscordButton.addActionListener(this);
+        c.add(launchDiscordButton);
+
+        launchGHubButton = new JButton("GHub", iconGHub);
+        launchGHubButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        launchGHubButton.setSize(175, 65); //icon 50x50
+        launchGHubButton.setLocation(900, 600);
+        launchGHubButton.addActionListener(this);
+        c.add(launchGHubButton);
+
+        balance = new JLabel("Balance : ");
+        balance.setFont(new Font("Arial", Font.PLAIN, 20));
+        balance.setSize(150,80);
+        balance.setLocation(1000,0);
+        c.add(balance);
+
+        balanceText = new JLabel("0");
+        balanceText.setFont(new Font("Arial", Font.PLAIN, 20));
+        balanceText.setSize(150,80);
+        balanceText.setLocation(1100,0);
+        c.add(balanceText);
+
+        balanceAddButton = new JButton("Deposit");
+        balanceAddButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        balanceAddButton.setSize(150    , 25);
+        balanceAddButton.setLocation(1000, 60);
+        balanceAddButton.addActionListener(this);
+        c.add(balanceAddButton);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -42,6 +148,9 @@ if(e.getSource()==launchSteamButton){
 }
     }
 
-
+    public static void main(String[] args) {
+        GameLibrary gameLibrary = new GameLibrary();
+        gameLibrary.setVisible(true);
+    }
 
 }
